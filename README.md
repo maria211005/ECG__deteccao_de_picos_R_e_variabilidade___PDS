9694bd17a4f8d28750c0a35426cc500813248dd0
duda_mafra <129239516+maria211005@users.noreply.github.com>
2025-12-05 22:52:08 -0300

    comecei a segunda parte e comentei o que fiz no readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ECG: detecção de picos R e variabilidade
 Este repositório faz parte da entrega do projeto final da matéria de Processamento Digital de Sinais no segundo semestre de 2025.
+
+## Como rodar o código?
+É necessário instalar as bibliotecas:
+### wfdb (que contém o banco de dados recoemndado para uso)
+### numpy (responsável por realizar operações matemáticas)
+### matplot ()
+### scipy (responsável pela filtragem, a qual será utilizada a butter (butterworth) e filtfilt)
+### neurokit2 e ts2vg (para detecção de picos R, análise de VFC e HRV, sendo a última uma biblioteca auxiliar para refinar o método de detecção dos picos)
+
+Para conseguir baixar as bibliotecas, é necessário realizar a instalação dos mesmos através do comando "pip install []", por exemplo
+### pip install wfdb 
+É possível fazer todas as instalações direto, como
+### pip install wfdb numpy matplot scipy neurokit2 ts2vg
--- projeto.py
+++ projeto.py
@@ -1,22 +1,24 @@
 import wfdb                                 # Biblioteca especializada para ler dados do PhysioNet, base de dados que foi coletada as ECGs (MIT-BIH)
 import numpy
 import matplotlib.pyplot as plt
-from scipy.signal import butter, filtfilt   # bibliotecas de filtragem  
+from scipy.signal import butter, filtfilt   # Bibliotecas de filtragem  
 import neurokit2                            # Biblioteca para detecção de picos R e análise de VFC/HRV
+import ts2vg                                # Biblioteca auxiliar para método de detecção de picos R
 
 #Objetivo
-#identificar automaticamente os picos R (deflexões mais proeminentes do complexo QRS) 
-#calcular os intervalos RR (tmepo entre batimentos)
-#derivar informaçoes sobre ritmo cardiaco e variabilidade
+# identificar automaticamente os picos R (deflexões mais proeminentes do complexo QRS) 
+# calcular os intervalos RR (tmepo entre batimentos)
+# derivar informaçoes sobre ritmo cardiaco e variabilidade
 
 #Processo
-#aplicar tecnicas para localizar as posições de picos R (derivação, limiar adaptativo - Pan Tompkins)
-#analise dos intervalos RR calcula frequencia cardíaca instantanea, BPM e variabilidade de ritmo cardiaco (HRV)
+# filtragem passa-banda (0,5Hz a 40Hz) para eliminar ruídos de baixa frequencia e interferência de rede elétrica (50/60Hz)
+# aplicar tecnicas para localizar as posições de picos R (derivação, limiar adaptativo - Pan Tompkins)
+# analise dos intervalos RR calcula frequencia cardíaca instantanea, BPM e variabilidade de ritmo cardiaco (HRV)
 
 #Resultados
-#sinal filtrado com sobreposição de picos R detectados
-#tabelas com valores médios e desvio padrão dos intervalos RR
-#gráficos temporais mostrando a variação de frequencia cardiaca ao longo do registro (histograma de distribuição dos intervalos RR e métricas de desempenho de detecção)
+# sinal filtrado com sobreposição de picos R detectados
+# tabelas com valores médios e desvio padrão dos intervalos RR
+# gráficos temporais mostrando a variação de frequencia cardiaca ao longo do registro (histograma de distribuição dos intervalos RR e métricas de desempenho de detecção)
 
 #Adicional
 #análise espectral de HRV para ilustrar o equilíbrio autonômico e discutir implicações fisiológicas
@@ -62,7 +64,7 @@ plt.plot(numpy.arange(len(sinal_filtrado)) / fs, sinal_filtrado, label='Filtrado
 plt.title('Comparação: Sinal Bruto vs. Sinal Filtrado (Passa-Banda)')
 plt.xlabel('Tempo (s)')
 plt.ylabel('Amplitude (mV)')
-plt.xlim(0, 5) # Zoom nos primeiros 5 segundos para melhor visualização
+plt.xlim(0, 10)
 plt.legend()
 plt.show()
 '''
@@ -72,7 +74,7 @@ plt.show()
 # baseado no detector de Koka, transforma o ECG em representação gráfica e extrai as posições exatas usando grafos
 _, info = neurokit2.ecg_peaks(sinal_filtrado, sampling_rate=fs, method='emrich2023')
 picosR = info["ECG_R_Peaks"]
-
+'''
 plt.figure(figsize=(12, 4))
 plt.plot(numpy.arange(len(sinal_filtrado)) / fs, sinal_filtrado)
 plt.plot(picosR / fs, sinal_filtrado[picosR], "o", color='red', markersize=5)
@@ -81,3 +83,30 @@ plt.xlabel('Tempo (s)')
 plt.ylabel('Amplitude (mV)')
 plt.xlim(0, 20)
 plt.show()
+'''
+#--------------------------------- VARIABILIDADE DA FREQUENCIA CARDIACA ---------------------------------
+
+#diferencas entre amostras e converter em milissegundos 
+diferencas = numpy.diff(picosR)
+intervalosRR = diferencas/fs
+FC_instantanea = 60/intervalosRR
+
+# calculo da frequencia cardiaca instantanea
+taxa_instantanea = neurokit2.ecg_rate(picosR, sampling_rate=fs, desired_length=len(sinal_filtrado))
+
+# Isso permite que a frequência instantânea seja alinhada ao sinal de ECG.
+ecg_processado, info_processado = neurokit2.ecg_process(sinal_filtrado, sampling_rate=fs)
+
+# A função nk.hrv calcula todas as métricas necessárias:
+hrv_metrics = neurokit2.hrv(picosR, sampling_rate=fs)
+
+# Extrair o BPM Médio
+bpm_medio = hrv_metrics['HRV_MeanHR'][0]
+
+print("\n--- Resultados de Ritmo Cardíaco e Variabilidade (HRV) ---")
+print(f"**Frequência Cardíaca Média (BPM Médio): {bpm_medio:.2f} BPM**")
+
+# Exibir as métricas de variabilidade
+print("\n--- Tabela de Métricas de VFC/HRV ---")
+# Usamos .T para transpor e facilitar a leitura das métricas
+print(hrv_metrics.T)
